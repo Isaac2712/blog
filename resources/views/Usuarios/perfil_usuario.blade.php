1ac6f9cfe640d9f97af11903488317b75709c349
Isaac Marroqui <isaac.marroc@gmail.com>
2019-05-06 13:22:16 +0200

    cookies.js

---
+++ resources/views/Usuarios/perfil_usuario.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('contenido')
+	<section class="container mt-4 mb-4">
+		<span> Session del usuario <span class="font-weight-bold"> <?= $usuario[0]['nick'] ?></span> </span>
+	</section>
+@stop
